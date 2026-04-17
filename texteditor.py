3571da31f44ca2e4bc363369293cdab420f2397f
Rahul Gupta <toorahulgupta@gmail.com>
2026-04-18 03:24:01 +0530

    added function for open file option

--- texteditor.py
+++ texteditor.py
@@ -5,3 +5,11 @@ from tkinter import filedialog, messagebox
 # Function to create a new file (clear text area)
 def newfile():
     text.delete(1.0, tk.END)
+
+# Function to open an existing file
+def openfile():
+    # Open file dialog to select a .txt file
+    file_path = filedialog.askopenfilename(
+        defaultextension=".txt",
+        filetypes=[("Text Files", "*.txt")]
+    )
